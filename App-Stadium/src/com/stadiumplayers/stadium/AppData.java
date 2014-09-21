54fc91f38e2d80d203d63ccd5d4b6a7f3d73729a
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2014-09-21 09:48:20 -0400

    approaching minutes

--- App-Stadium/src/com/stadiumplayers/stadium/AppData.java
+++ App-Stadium/src/com/stadiumplayers/stadium/AppData.java
@@ -1,36 +1,53 @@
 
 package com.stadiumplayers.stadium;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public enum AppData {
 
     INSTANCE;
+
+    private static final String SHARED_PREF_LOGIN = "loginstate";
+    private static final String KEY_LOGGED_IN = "keyLoggedIn";
+    private static final String KEY_USER_ID = "keyUserId";
+    private static final String KEY_FIRST_NAME = "keyFirstName";
+    private static final String KEY_LAST_NAME = "keyLastName";
 
     private long mUserId;
     private String mFirstName;
     private String mLastName;
     private boolean mLoggedIn = false;
 
-    public static void init(long userId, String firstName, String lastName, boolean loggedIn) {
-        INSTANCE.mUserId = userId;
-        INSTANCE.mFirstName = firstName;
-        INSTANCE.mLastName = lastName;
-        INSTANCE.mLoggedIn = loggedIn;
+    public static void init(long userId, String firstName, String lastName,
+            boolean loggedIn) {
+        
+        getSharedPref().edit()
+                .putLong(KEY_USER_ID, userId)
+                .putString(KEY_FIRST_NAME, firstName)
+                .putString(KEY_LAST_NAME, lastName)
+                .putBoolean(KEY_LOGGED_IN, loggedIn)
+                .commit();
     }
     
+    private static SharedPreferences getSharedPref() {
+        return App.getContext().getSharedPreferences(SHARED_PREF_LOGIN, Context.MODE_PRIVATE);
+    }
+
     public static Long getUserId() {
-        return INSTANCE.mUserId;
+        return getSharedPref().getLong(KEY_USER_ID, 0);
     }
 
     public static String getFirstName() {
-        return INSTANCE.mFirstName;
+        return getSharedPref().getString(KEY_FIRST_NAME, "null");
     }
-    
+
     public static String getLastName() {
-        return INSTANCE.mLastName;
+        return getSharedPref().getString(KEY_LAST_NAME, "null");
     }
-    
+
     public static boolean isLoggedIn() {
-        return INSTANCE.mLoggedIn;
+        return getSharedPref().getBoolean(KEY_LOGGED_IN, false);
     }
 
 }
--- App-Stadium/src/com/stadiumplayers/stadium/activities/ActivitySplashPage.java
+++ App-Stadium/src/com/stadiumplayers/stadium/activities/ActivitySplashPage.java
@@ -1,7 +1,9 @@
+
 package com.stadiumplayers.stadium.activities;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -13,50 +15,55 @@ import com.facebook.SessionState;
 import com.facebook.model.GraphUser;
 import com.stadiumplayers.stadium.AppData;
 import com.stadiumplayers.stadium.R;
-import com.stadiumplayers.stadium.dialogs.DialogLogin;
 import com.stadiumplayers.stadium.dialogs.DialogLogin.OnDialogLoginListener;
 
-public class ActivitySplashPage extends FragmentActivity implements OnClickListener, OnDialogLoginListener {
-    
+public class ActivitySplashPage extends FragmentActivity implements OnClickListener,
+        OnDialogLoginListener {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashpage);
-        
+
         Button btnLogin = (Button) findViewById(R.id.splashpage_btn_login);
         Button btnBrowser = (Button) findViewById(R.id.splashpage_btn_browse);
-        
+
         btnLogin.setOnClickListener(this);
         btnBrowser.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        
+
         switch (view.getId()) {
         case R.id.splashpage_btn_login:
             Session.openActiveSession(this, true, new Session.StatusCallback() {
 
                 @Override
                 public void call(Session session, SessionState state, Exception exception) {
+
+                    Log.d("ryan", "making fb call");
                     
                     // If we logged in
                     if (session.isOpened()) {
 
-                        // Create a facebook request for the logged in user's info
+                        Log.d("ryan", "session is open...");
+                        // Create a facebook request for the logged in user's
+                        // info
                         Request.newMeRequest(session, new Request.GraphUserCallback() {
 
                             @Override
                             public void onCompleted(GraphUser user, Response response) {
-                                
+
+                                Log.d("ryan", "finished request");
                                 // If the request was successful
                                 if (user != null) {
-                                    
+                                    Log.d("ryan", "not null");
                                     // Save the user information
-                                    Log.e("ryan", "got da info");
                                     AppData.init(Long.parseLong(user.getId()), user.getFirstName(),
                                             user.getLastName(), true);
                                     ActivitySplashPage.this.finish();
+                                    Log.d("ryan", "finished activity");
                                 }
                             }
                         });
@@ -64,7 +71,7 @@ public class ActivitySplashPage extends FragmentActivity implements OnClickListe
                 }
             });
             break;
-            
+
         case R.id.splashpage_btn_browse:
             ActivitySplashPage.this.finish();
             break;
@@ -74,20 +81,19 @@ public class ActivitySplashPage extends FragmentActivity implements OnClickListe
     @Override
     public void onLoginSuccessful() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void onLoginFailure() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void onKeepBrowsing() {
         // TODO Auto-generated method stub
-        
+
     }
-    
 
 }
